cb2e4118f9d32e1253899f6f53cc9001b059b657
Pedro Fouto <pfouto10@gmail.com>
2018-03-05 20:40:48 +0000

    stuff

--- cassandra/src/main/java/com/yahoo/ycsb/db/KeyspaceManager.java
+++ cassandra/src/main/java/com/yahoo/ycsb/db/KeyspaceManager.java
@@ -96,7 +96,7 @@ public class KeyspaceManager {
               InetAddress.getLocalHost(), -1, System.currentTimeMillis());
           ConnectionManager.getConnectionToHigh(InetAddress.getByName(CassandraCQLClient.internals.get(currentDc))).writeAndFlush(mm);
 
-          MigrateMessage take = CassandraCQLClient.migrateResponses.get(Thread.currentThread().getId()).poll(120, TimeUnit.SECONDS);
+          MigrateMessage take = CassandraCQLClient.migrateResponses.get(Thread.currentThread().getId()).poll(240, TimeUnit.SECONDS);
           if(take == null){
             System.err.println("timeout migrating: " + mm);
             System.out.println("timeout migrating: " + mm);
